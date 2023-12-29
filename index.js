0234279b4ae47d671e331aeb988c9c0535f6b66f
OumaArera <johnouma999@gmail.com>
2023-12-29 22:19:58 +0300

    feat: rearrange number in descending order

---
+++ index.js
@@ -0,0 +1,9 @@
+/* 
+ Takes an integer
+ Converts it to string
+ Splits it into pieces
+ Sorts in a descending order
+ Joins the sorted string
+ Converts it back to integer
+*/ 
+const descendingOrder = n => parseInt(n.toString().split("").sort((a,b) => b-a).join(""));
