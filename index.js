aa8abeb026e370c4307fa7d58a2d6ad59a5b05ed
OumaArera <johnouma999@gmail.com>
2023-12-29 22:26:06 +0300

    feat: rearrange numbers in ascending order

--- index.js
+++ index.js
@@ -7,3 +7,13 @@
  Converts it back to integer
 */ 
 const descendingOrder = n => parseInt(n.toString().split("").sort((a,b) => b-a).join(""));
+
+/* 
+ Takes an integer
+ Converts it to string
+ Splits it into pieces
+ Sorts in a ascending order
+ Joins the sorted string
+ Converts it back to integer
+*/ 
+const ascendingOrder = n => parseInt(n.toString().split("").sort((a,b) => a-b).join(""));
